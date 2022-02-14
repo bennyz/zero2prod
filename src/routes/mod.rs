d2f2f465f1b6512408dd6e6234db9756ee0fdec9
Benny Zlotnik <bzlotnik@protonmail.com>
2022-02-14 13:16:26 +0200

    use SetRequestId to generate the x-request-id header

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,13 +3,16 @@ mod subscriptions;
 
 use axum::AddExtensionLayer;
 pub use health_check::*;
+use hyper::header::HeaderName;
 use hyper::{Body, Request};
 use sqlx::PgPool;
 pub use subscriptions::*;
 
 use axum::routing::post;
 use axum::{self, routing::get, Router};
-use tower_http::request_id::{MakeRequestId, RequestId};
+use tower_http::request_id::{
+    MakeRequestId, PropagateRequestIdLayer, RequestId, SetRequestIdLayer,
+};
 use tower_http::trace::TraceLayer;
 use uuid::Uuid;
 
@@ -19,20 +22,32 @@ pub struct ApiContext {
 }
 
 pub fn app(db: PgPool) -> Router {
+    let x_request_id = HeaderName::from_static("x-request-id");
     Router::new()
         .route("/health_check", get(health_check))
         .route("/subscriptions", post(subscribe))
         .layer(AddExtensionLayer::new(ApiContext { db }))
         .layer(
             TraceLayer::new_for_http().make_span_with(|request: &Request<Body>| {
+                let request_id = request
+                    .extensions()
+                    .get::<RequestId>()
+                    .and_then(|id| id.header_value().to_str().ok())
+                    .unwrap_or_default();
+
                 tracing::info_span!(
                     "HTTP",
                     http.method = %request.method(),
                     http.url = %request.uri(),
-                    request_id = %Uuid::new_v4(),
+                    request_id = %request_id,
                 )
             }),
         )
+        .layer(SetRequestIdLayer::new(
+            x_request_id.clone(),
+            MakeRequestUuid,
+        ))
+        .layer(PropagateRequestIdLayer::new(x_request_id))
 }
 
 #[derive(Clone, Copy)]
